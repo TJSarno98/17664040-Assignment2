87a6b9d56c665d34bedde82fb3432521fd58e454
Thomas Sarno <TJSarno98@gmail.com>
2019-01-08 16:01:13 +0000

    Updated canvas to include background image and started to add buttons.

--- JS/canvas.js
+++ JS/canvas.js
@@ -1,12 +1,18 @@
 var canvas = document.querySelector('canvas');
 
-canvas.height = 500;
-canvas.width= 500;
+canvas.height = 439;
+canvas.width= 1023;
 
+// Setting a background image and having it load in when the page is loaded.
 var background = new Image();
+background.src="./Assets/shirt_template.jpg"
+background.onload = function(){
+  context.drawImage(background,0,0)
+}
 
 var context = canvas.getContext('2d');
 var down = false;
+context.lineWidth= 5;
 
 canvas.addEventListener('mousemove', draw);
 
@@ -31,14 +37,36 @@ function draw(e){
     context.stroke();
   }
 }
-
-canvas.addEventListener('touchstart',function(){
-  down = true;
+//Adding a function to clear canvas
+function clear_canvas(){
   context.beginPath();
-  context.moveTo(xPos, Ypos);
-  canvas.addEventListener("touchmove",draw);
-});
+   context.clearRect(0,0,canvas.width,canvas.height);
+   context.drawImage(background,0,0)
+ }
 
-canvas.addEventListener('touchend', function(){
-  down = false;
-})
+//Adding functions to control line width
+function size_small(){
+  context.lineWidth = 1;
+}
+
+function size_medium(){
+  context.lineWidth = 3;
+}
+
+//Adding functions to control line colour.
+function colour_red(){
+  context.strokeStyle ="#ff0000"
+}
+
+
+
+// canvas.addEventListener('touchstart',function(){
+//   down = true;
+//   context.beginPath();
+//   context.moveTo(xPos, Ypos);
+//   canvas.addEventListener("touchmove",draw);
+// });
+//
+// canvas.addEventListener('touchend', function(){
+//   down = false;
+// })
