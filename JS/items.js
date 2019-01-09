c526ff7652119750157bc6ee7ee03781fee45173
Thomas Sarno <TJSarno98@gmail.com>
2019-01-09 19:38:43 +0000

    Added audio player with multiple file formats. Also added video backup.

--- JS/items.js
+++ JS/items.js
@@ -1,13 +1,13 @@
 // Using Jquery to manage a basic cart
 
-//Adding the price of an item.
+//Adding the price of an item and a quantity.
 $(document).ready(function() {
 $('.add').click(function() {
    $('#total').text(parseInt($('#total').text()) + parseInt($(this).data('amount')));
    $('#totalitems').text(parseInt($('#totalitems').text()) + parseInt($(this).data('quantity')));
 });
 })
-//Subtracting the price of an item.
+//Subtracting the price of an item and a quantity.
 $(document).ready(function() {
 $('.subtract').click(function() {
    $('#total').text(parseInt($('#total').text()) + parseInt($(this).data('amount')));
